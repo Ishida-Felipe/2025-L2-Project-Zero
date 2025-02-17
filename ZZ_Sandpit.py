c9e540b4fe68c86e924f412a31b8badebcbad024
Ishida-Felipe <felipeyudi.ishida@horowhenua.school.nz>
2025-02-17 15:05:55 +1300

    Simple Yes/No String Checker and Not Blank

---
+++ ZZ_Sandpit.py
@@ -0,0 +1,14 @@
+# reminder - here is an integer that has been multiplied by 3
+number = 5
+print(number * 3)
+
+# here is a string tht has been multiplied by 3
+# (Note the speech marks and text colour)
+num_string = "5"
+print(num_string * 3)
+
+# An example showing what the leg(foo) method does
+example_text = "hello world, Brasil!"
+text_length = len(example_text)
+
+print(f"'{example_text}' is {text_length} caracters long")
