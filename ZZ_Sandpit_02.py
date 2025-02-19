4239263e795ac14457789d5d20837595e35faf4c
Ishida-Felipe <felipeyudi.ishida@horowhenua.school.nz>
2025-02-20 11:45:09 +1300

    String Check First Letter/Word

---
+++ ZZ_Sandpit_02.py
@@ -0,0 +1,16 @@
+# list exploration / experiment
+
+fruit_list = ['apple', 'banana', 'cherry', 'dragon fruit']
+
+for item in fruit_list:
+
+    print()
+
+    # print the whole word
+    print("Fruit Name: ", item)
+
+    # print the first letter
+    print("First Letter: ", item[0])
+
+    # print the first two letters
+    print("First and second Letter: ", item[:2])
